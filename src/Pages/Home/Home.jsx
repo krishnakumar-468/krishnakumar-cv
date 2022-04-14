bcdd654bf081ba26ff44662c3e2b871cada5a24a
krishnakumar <krishna.kumar@hotelcard.com>
2022-04-14 22:31:21 +0530

    animations

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className="container home-container">
             <div className="home-text">
-                <h1>Hy,  I AM <br /> KRISHNAKUMAR</h1>
+                <h1>Hy,  I AM <br /> <span>KRISHNAKUMAR</span> </h1>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                 <Button text="Download CV"></Button>
             </div>
