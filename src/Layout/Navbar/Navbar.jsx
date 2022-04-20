c14d30b8d008f02745d369037324875fedd4061f
krishnakumar <krishna.kumar@hotelcard.com>
2022-04-20 18:43:23 +0530

    navbar mobile

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Button from '../../Components/Button/Button';
 import './Navbar.css';
 import About from '../../Pages/About/About';
@@ -13,35 +14,40 @@ import{
 
 
 function Navbar() {
+    const [show, setShow] = React.useState();
     return (
         <BrowserRouter>
             <div className="navbar container">
                 <div className="logo">
                     <Link  to="krishnakumar-cv/"><img src={process.env.PUBLIC_URL + '/images/logo.png'} alt="logo" /></Link>
                 </div>
-                <div className='nave-link'>
-                    <div className="nave-link-menu">
+                <div className={show ? "show" : ""} >
+                    <div  className="nave-link">
+                        <div className="nave-link-menu">
+                            
+                                <ul>
+                                    <li onClick={() => setShow(false)}><Link  to="krishnakumar-cv/">Home</Link></li>
+                                    <li onClick={() => setShow(false)}><Link to="krishnakumar-cv/About">About me   </Link> </li>
+                                    <li onClick={() => setShow(false)}><Link to="krishnakumar-cv/Portfolio"> Portfolio</Link> </li>
+                                    {/* <li> Skills </li> */}
+                                    <li onClick={() => setShow(false)}><Link to="krishnakumar-cv/Experience">Experience </Link></li>
+                                </ul>
+                                <span class="material-icons" onClick={() => setShow(false)}>close</span>
+                        </div>
                         
-                            <ul>
-                                <li><Link  to="krishnakumar-cv/">Home</Link></li>
-                                <li><Link to="krishnakumar-cv/About">About me   </Link> </li>
-                                <li><Link to="krishnakumar-cv/Portfolio"> Portfolio</Link> </li>
-                                {/* <li> Skills </li> */}
-                                <li><Link to="krishnakumar-cv/Experience">Experience </Link></li>
-                            </ul>
-                    </div>
-                    
-                    <div>
-                        <Button text="Download CV"></Button>
-                    </div>
+                        <div>
+                            <Button text="Download CV"></Button>
+                        </div>
 
-                    <div className="hamb-menu">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
+                        <div className="hamb-menu" onClick={() => setShow(true)}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
 
+                    </div>
                 </div>
+                
             </div>
             <Routes>
                 <Route exact path="krishnakumar-cv/" element={<Home />} /> 
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
@@ -8,24 +9,116 @@ function Portfolio() {
                 <div className="home-text">
                     <h3>My portfolio</h3>
                 </div>
-                <div className="por-wrapp">
+                <div className="pro-nav">
+                    <a href='#pro-design-web' className="pro-nav-link">
+                       Web
+                    </a>
+                    <a href='#pro-design-app' className="pro-nav-link">
+                       Angular
+                    </a>
+                    <a href='#pro-design-react' className="pro-nav-link">
+                       React
+                    </a>
+                    <a href='#pro-design' className="pro-nav-link">
+                       Design
+                    </a>
+                </div>
+                <h3 className="id-h3" id="pro-design-web">Web</h3>
+                <div className="por-wrapp" >
+                    <div className="por-wrapp-col">
+                        <a href='https://hotelcard.com/' className="pro-nav-link" target="_blank">
+                        Hotelcard
+                        </a>
+                    </div>
+                    <div className="por-wrapp-col">
+                        <a href='http://xamion.in' className="pro-nav-link" target="_blank">
+                        Xamion
+                        </a>
+                    </div>
+                    <div className="por-wrapp-col">
+                        <a href='http://web.accoxi.com' className="pro-nav-link" target="_blank">
+                        Accoxi
+                        </a>
+                    </div>
+                    <div className="por-wrapp-col">
+                        <a href='http://at228.ofabi.com/zelloznew/index.html#' className="pro-nav-link" target="_blank">
+                        Zelloz
+                        </a>
+                    </div>
+                    <div className="por-wrapp-col">
+                        <a href='http://www.bdmconsultancy.com/' className="pro-nav-link" target="_blank">
+                        BDM consultancy
+                        </a>
+                    </div>
+                    <div className="por-wrapp-col">
+                        <a href='http://gentlemanchits.com/' className="pro-nav-link" target="_blank">
+                        Gentleman chits
+                        </a>
+                    </div>
+                    <div className="por-wrapp-col">
+                        <a href='http://www.snsgov-shj.ae/' className="pro-nav-link" target="_blank">
+                        SNSGov
+                        </a>
+                    </div>
+                    <div className="por-wrapp-col">
+                        <a href='https://nakshathraonline.com/' className="pro-nav-link" target="_blank">
+                        Nakshathra
+                        </a>
+                    </div>
+                </div>
+                <h3 className="id-h3" id="pro-design-app">App - Angular</h3>
+                <div className="por-wrapp" >
+                    
+                    <div className="por-wrapp-col por-wrapp-col100">
+                        <LazyLoadImage src={process.env.PUBLIC_URL + '/images/design/po-13.jpg'} alt="works"></LazyLoadImage>
+                    </div>
+                </div>
+
+                <h3 className="id-h3" id="pro-design-react">My portfolio</h3>
+                <div className="por-wrapp" >
+                    <a href='https://krishnakumar-468.github.io/krishnakumar-cv/' className="pro-nav-link" target="_blank">
+                       My portfolio
+                    </a>
+                </div>
+
+                <h3 className="id-h3" id="pro-design">Design</h3>
+                <div className="por-wrapp" >
+                    
                     <div className="por-wrapp-col">
                         <LazyLoadImage src={process.env.PUBLIC_URL + '/images/design/po-1.jpg'} alt="works"></LazyLoadImage>
                     </div>
                     <div className="por-wrapp-col">
-                        <img src={process.env.PUBLIC_URL + '/images/design/po-2.jpg'} alt="works" />
+                        <LazyLoadImage src={process.env.PUBLIC_URL + '/images/design/po-2.jpg'} alt="works"></LazyLoadImage>
                     </div>
                     <div className="por-wrapp-col">
-                        <img src={process.env.PUBLIC_URL + '/images/design/po-3.jpg'} alt="works" />
+                        <LazyLoadImage src={process.env.PUBLIC_URL + '/images/design/po-3.jpg'} alt="works"></LazyLoadImage>
                     </div>
                     <div className="por-wrapp-col">
-                        <img src={process.env.PUBLIC_URL + '/images/design/po-4.jpg'} alt="works" />
+                        <LazyLoadImage src={process.env.PUBLIC_URL + '/images/design/po-4.jpg'} alt="works"></LazyLoadImage>
                     </div>
                     <div className="por-wrapp-col">
-                        <img src={process.env.PUBLIC_URL + '/images/design/po-5.jpg'} alt="works" />
+                        <LazyLoadImage src={process.env.PUBLIC_URL + '/images/design/po-5.jpg'} alt="works"></LazyLoadImage>
                     </div>
                     <div className="por-wrapp-col">
-                     <LazyLoadImage effect="blur" src={process.env.PUBLIC_URL + '/images/design/po-1.jpg'} alt="works"></LazyLoadImage>
+                     <LazyLoadImage effect="blur" src={process.env.PUBLIC_URL + '/images/design/po-6.jpg'} alt="works"></LazyLoadImage>
+                    </div>
+                    <div className="por-wrapp-col">
+                     <LazyLoadImage effect="blur" src={process.env.PUBLIC_URL + '/images/design/po-7.jpg'} alt="works"></LazyLoadImage>
+                    </div>
+                    <div className="por-wrapp-col">
+                     <LazyLoadImage effect="blur" src={process.env.PUBLIC_URL + '/images/design/po-8.jpg'} alt="works"></LazyLoadImage>
+                    </div>
+                    <div className="por-wrapp-col">
+                     <LazyLoadImage effect="blur" src={process.env.PUBLIC_URL + '/images/design/po-9.jpg'} alt="works"></LazyLoadImage>
+                    </div>
+                    <div className="por-wrapp-col">
+                     <LazyLoadImage effect="blur" src={process.env.PUBLIC_URL + '/images/design/po-10.jpg'} alt="works"></LazyLoadImage>
+                    </div>
+                    <div className="por-wrapp-col">
+                     <LazyLoadImage effect="blur" src={process.env.PUBLIC_URL + '/images/design/po-11.jpg'} alt="works"></LazyLoadImage>
+                    </div>
+                    <div className="por-wrapp-col">
+                     <LazyLoadImage effect="blur" src={process.env.PUBLIC_URL + '/images/design/po-12.jpg'} alt="works"></LazyLoadImage>
                     </div>
                     
                 </div>
